64b5b8762b4effe2655bce6f82d3ebd0d922d739
djagruthi-coder <d29jagruthi@gmail.com>
2025-09-05 12:26:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st 
 st.title("smart agri app")
-st..write("hello, smart India Hackthon!")
+st.write("hello, smart India Hackthon!")
 st.write("this is our first streamlit app for agricutlure solutions")
